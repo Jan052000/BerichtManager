9f9ae21ed5cc88ee57b53136d4b1e1a4d42dc33e
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2023-11-10 15:06:54 +0100

    add themed message box

---
+++ BerichtManager/OwnControls/ThemedMessageBox.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace BerichtManager.OwnControls
+{
+	partial class ThemedMessageBox
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btCancel = new System.Windows.Forms.Button();
+			this.btNo = new System.Windows.Forms.Button();
+			this.paText = new System.Windows.Forms.Panel();
+			this.rtbText = new System.Windows.Forms.RichTextBox();
+			this.btYes = new System.Windows.Forms.Button();
+			this.paText.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btCancel
+			// 
+			this.btCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btCancel.Location = new System.Drawing.Point(347, 226);
+			this.btCancel.Name = "btCancel";
+			this.btCancel.Size = new System.Drawing.Size(75, 23);
+			this.btCancel.TabIndex = 0;
+			this.btCancel.Text = "Cancel";
+			this.btCancel.UseVisualStyleBackColor = true;
+			// 
+			// btNo
+			// 
+			this.btNo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btNo.Location = new System.Drawing.Point(266, 226);
+			this.btNo.Name = "btNo";
+			this.btNo.Size = new System.Drawing.Size(75, 23);
+			this.btNo.TabIndex = 1;
+			this.btNo.Text = "No";
+			this.btNo.UseVisualStyleBackColor = true;
+			// 
+			// paText
+			// 
+			this.paText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.paText.Controls.Add(this.rtbText);
+			this.paText.Location = new System.Drawing.Point(12, 12);
+			this.paText.Name = "paText";
+			this.paText.Size = new System.Drawing.Size(410, 208);
+			this.paText.TabIndex = 2;
+			// 
+			// rtbText
+			// 
+			this.rtbText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.rtbText.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.rtbText.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.rtbText.Location = new System.Drawing.Point(0, 0);
+			this.rtbText.Name = "rtbText";
+			this.rtbText.ReadOnly = true;
+			this.rtbText.Size = new System.Drawing.Size(410, 208);
+			this.rtbText.TabIndex = 2;
+			this.rtbText.Text = "";
+			// 
+			// btYes
+			// 
+			this.btYes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btYes.Location = new System.Drawing.Point(185, 226);
+			this.btYes.Name = "btYes";
+			this.btYes.Size = new System.Drawing.Size(75, 23);
+			this.btYes.TabIndex = 3;
+			this.btYes.Text = "Yes";
+			this.btYes.UseVisualStyleBackColor = true;
+			// 
+			// ThemedMessageBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(434, 261);
+			this.Controls.Add(this.btYes);
+			this.Controls.Add(this.paText);
+			this.Controls.Add(this.btNo);
+			this.Controls.Add(this.btCancel);
+			this.Name = "ThemedMessageBox";
+			this.Text = "ColoredMessageBox";
+			this.paText.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btCancel;
+		private System.Windows.Forms.Button btNo;
+		private System.Windows.Forms.Panel paText;
+		private System.Windows.Forms.RichTextBox rtbText;
+		private System.Windows.Forms.Button btYes;
+	}
+}
---
+++ BerichtManager/OwnControls/ThemedMessageBox.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using BerichtManager.ThemeManagement;
+using BerichtManager.ThemeManagement.DefaultThemes;
+
+namespace BerichtManager.OwnControls
+{
+	public partial class ThemedMessageBox : Form
+	{
+		/// <summary>
+		/// Message inside of <see cref="rtbText"/>
+		/// </summary>
+		private string Message { get; set; }
+		/// <summary>
+		/// Title in title bar
+		/// </summary>
+		private string Title { get; set; }
+		/// <summary>
+		/// Configuration of buttons
+		/// </summary>
+		private MessageBoxButtons Buttons { get; set; }
+
+		private ThemedMessageBox(ITheme theme, string text = "", string title = "", MessageBoxButtons buttons = MessageBoxButtons.OK)
+		{
+			InitializeComponent();
+			InitializeButtons(buttons);
+			this.Icon = Icon.ExtractAssociatedIcon(Path.GetFullPath(".\\BerichtManager.exe"));
+			if (theme == null) theme = new DarkMode();
+			ThemeSetter.SetThemes(this, theme);
+			this.Text = title;
+			rtbText.Text = text;
+			Message = text;
+			Title = title;
+			Buttons = buttons;
+			AutoSizeBox();
+			rtbText.SelectAll();
+			rtbText.SelectionAlignment = HorizontalAlignment.Center;
+			rtbText.DeselectAll();
+			rtbText.Enter += UnfocusOnEnter;
+		}
+
+		/// <summary>
+		/// Shows a themed message box
+		/// </summary>
+		/// <param name="theme">Theme to style the message box after</param>
+		/// <param name="text">Text to be displayed on the message box</param>
+		/// <param name="title">Title of the message box</param>
+		/// <param name="buttons">Configuration of the buttons on message box</param>
+		/// <returns></returns>
+		public static DialogResult Show(ITheme theme, string text = "", string title = "", MessageBoxButtons buttons = MessageBoxButtons.OK)
+		{
+			return new ThemedMessageBox(theme, text: text, title: title, buttons: buttons).ShowDialog();
+		}
+
+		/// <summary>
+		/// Method to unfocus text box
+		/// </summary>
+		/// <param name="sender">Sender of event</param>
+		/// <param name="e">Event arguments</param>
+		private void UnfocusOnEnter(object sender, EventArgs e)
+		{
+			((Control)sender).Parent.Focus();
+		}
+
+		/// <summary>
+		/// Calculates and sets the width and height of the message box
+		/// </summary>
+		private void AutoSizeBox()
+		{
+			using (Graphics g = rtbText.CreateGraphics())
+			{
+				SizeF size = g.MeasureString(Message, rtbText.Font);
+				int titleBarHeight = 32;
+				int titleBarWidth = 8;
+				int paddingBottom = 20;
+				int paddingTop = 10;
+				int messageBoxWidth = titleBarWidth * 2 + (int)size.Width + Padding.Left + Padding.Right + rtbText.Margin.Left + rtbText.Margin.Right;
+				int messageBoxHeight = titleBarHeight + (int)size.Height + Padding.Top + Padding.Bottom + rtbText.Margin.Top + rtbText.Margin.Bottom + btYes.Padding.Top + btYes.Padding.Bottom + btYes.Height + paddingBottom + paddingTop;
+				if (Width < messageBoxWidth) Width = messageBoxWidth;
+				if (Height < messageBoxHeight) Height = messageBoxHeight;
+			}
+		}
+
+		/// <summary>
+		/// Sets texts of buttons and click events using <see cref="MessageBoxButtons"/>
+		/// </summary>
+		/// <param name="buttons">Configuration of buttons</param>
+		private void InitializeButtons(MessageBoxButtons buttons)
+		{
+			switch (buttons)
+			{
+				case MessageBoxButtons.OK:
+					btYes.Enabled = false;
+					btYes.Visible = false;
+					btNo.Enabled = false;
+					btNo.Visible = false;
+					btCancel.Text = "Ok";
+					btCancel.Click += OkClicked;
+					break;
+				case MessageBoxButtons.OKCancel:
+					btYes.Enabled = false;
+					btYes.Visible = false;
+					btNo.Text = "Ok";
+					btNo.Click += OkClicked;
+					btCancel.Click += CancelClicked;
+					break;
+				case MessageBoxButtons.AbortRetryIgnore:
+					btYes.Text = "Abort";
+					btYes.Click += AbortClicked;
+					btNo.Text = "Retry";
+					btNo.Click += RetryClicked;
+					btCancel.Text = "Ignore";
+					btCancel.Click += IgnoreClicked;
+					break;
+				case MessageBoxButtons.YesNoCancel:
+					btYes.Click += YesClicked;
+					btNo.Click += NoClicked;
+					btCancel.Click += CancelClicked;
+					break;
+				case MessageBoxButtons.YesNo:
+					btYes.Enabled = false;
+					btYes.Visible = false;
+					btNo.Text = "Yes";
+					btNo.Click += YesClicked;
+					btCancel.Text = "No";
+					btCancel.Click += NoClicked;
+					break;
+				case MessageBoxButtons.RetryCancel:
+					btYes.Enabled = false;
+					btYes.Visible = false;
+					btNo.Text = "Retry";
+					btNo.Click += RetryClicked;
+					btCancel.Text = "Cancel";
+					btCancel.Click += CancelClicked;
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void OkClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void CancelClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void AbortClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Abort;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void RetryClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Retry;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void IgnoreClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Ignore;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void YesClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Yes;
+			Close();
+		}
+
+		/// <summary>
+		/// Sets dialogresult
+		/// </summary>
+		private void NoClicked(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.No;
+			Close();
+		}
+	}
+}
