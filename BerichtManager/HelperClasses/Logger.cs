013106c55e7de842d3b7600206476d5176e1d604
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-02-23 15:38:59 +0100

    change logger to return log path

--- BerichtManager/HelperClasses/Logger.cs
+++ BerichtManager/HelperClasses/Logger.cs
@@ -7,24 +7,32 @@ namespace BerichtManager.HelperClasses
 {
 	internal class Logger
 	{
-		public static void LogError(Exception ex, JObject additionalData = null)
+		/// <summary>
+		/// Writes an <paramref name="ex"/> to a file
+		/// </summary>
+		/// <param name="ex"><see cref="Exception"/> to log</param>
+		/// <param name="additionalData">Additional data contained in exception</param>
+		/// <returns>Full path the log was saved to</returns>
+		public static string LogError(Exception ex, JObject additionalData = null)
 		{
 			string logFolder = Path.GetFullPath(".\\Logs");
 			string errorDate = DateTime.Now.ToString("dd.M.yyyy.H.m.s");
+			string filePath = logFolder + "\\" + errorDate + ".txt";
 			if (!Directory.Exists(logFolder))
 			{
 				Directory.CreateDirectory(logFolder);
 			}
 			if (additionalData != null)
 			{
-				File.WriteAllText(logFolder + "\\" + errorDate + ".txt", ex.Message
+				File.WriteAllText(filePath, ex.Message
 				+ "\n" + ex.StackTrace.ToString() + "\n" + ex.HResult + "\n\n" + JsonConvert.SerializeObject(additionalData));
 			}
-			else 
+			else
 			{
-				File.WriteAllText(logFolder + "\\" + errorDate + ".txt", ex.Message
+				File.WriteAllText(filePath, ex.Message
 				+ "\n" + ex.StackTrace.ToString() + "\n" + ex.HResult);
 			}
+			return filePath;
 		}
 	}
 }
