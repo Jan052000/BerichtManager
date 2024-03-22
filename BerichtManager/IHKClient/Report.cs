4fbc54c0816e0d10bef695651caf6522d874f6b0
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-03-22 11:58:35 +0100

    add classes to hold report content for sending

---
+++ BerichtManager/IHKClient/Report.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BerichtManager.IHKClient
+{
+	internal class Report
+	{
+		/// <summary>
+		/// File path of report
+		/// </summary>
+		public string ReportPath { get; set; }
+		/// <summary>
+		/// Number of report
+		/// </summary>
+		public int ReportNr { get; set; }
+		/// <summary>
+		/// Contents of report as they would be sent to IHK
+		/// </summary>
+		public ReportContent ReportContent { get; set; }
+
+		/// <summary>
+		/// Creates a new object of <see cref="Report"/>
+		/// </summary>
+		/// <param name="reportContent">Contents of the report</param>
+		public Report(ReportContent reportContent = null)
+		{
+			ReportContent = reportContent ?? new ReportContent();
+		}
+	}
+}
