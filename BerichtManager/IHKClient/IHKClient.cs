054b80e7faf06a65dd675546baa7d5e77c442d42
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-04-09 13:35:52 +0200

    add function to upload a report to IHK

--- BerichtManager/IHKClient/IHKClient.cs
+++ BerichtManager/IHKClient/IHKClient.cs
@@ -5,8 +5,11 @@ using System.Net;
 using BerichtManager.Config;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using BerichtManager.IHKClient.Exceptions;
+using Word = Microsoft.Office.Interop.Word;
+using BerichtManager.HelperClasses;
 
 namespace BerichtManager.IHKClient
 {
@@ -42,6 +45,7 @@ namespace BerichtManager.IHKClient
 			HttpClientHandler.CookieContainer = CookieContainer;
 			HttpClientHandler.UseCookies = true;
 			HttpClient.BaseAddress = new Uri(BASEURL);
+			HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0");
 		}
 
 		/// <summary>
@@ -62,6 +66,7 @@ namespace BerichtManager.IHKClient
 		/// <returns><see cref="HttpResponseMessage"/> of request</returns>
 		private async Task<HttpResponseMessage> PostAndRefer(string uri, HttpContent content)
 		{
+			//await Task.Delay(200 + new Random().Next(-50, 50));
 			HttpResponseMessage response = await HttpClient.PostAsync(uri, content);
 			HttpClient.DefaultRequestHeaders.Referrer = FromRelativeUri(uri);
 			return response;
@@ -70,12 +75,26 @@ namespace BerichtManager.IHKClient
 		/// <summary>
 		/// Sends an async get request using <see cref="HttpClient"/> and sets the referer to the <paramref name="uri"/> path
 		/// </summary>
-		/// <param name="content">Content to be posted</param>
+		/// <param name="uri">Relative path</param>
 		/// <returns><see cref="HttpResponseMessage"/> of request</returns>
 		private async Task<HttpResponseMessage> GetAndRefer(string uri)
 		{
+			//await Task.Delay(200 + new Random().Next(-50, 50));
 			HttpResponseMessage response = await HttpClient.GetAsync(uri);
 			HttpClient.DefaultRequestHeaders.Referrer = FromRelativeUri(uri);
+			return response;
+		}
+
+		/// <summary>
+		/// Sends an async get request using <see cref="HttpClient"/> and sets the referer to the <paramref name="uri"/> path
+		/// </summary>
+		/// <param name="uri">Uri</param>
+		/// <returns><see cref="HttpResponseMessage"/> of request</returns>
+		private async Task<HttpResponseMessage> GetAndRefer(Uri uri)
+		{
+			//await Task.Delay(200 + new Random().Next(-50, 50));
+			HttpResponseMessage response = await HttpClient.GetAsync(uri);
+			HttpClient.DefaultRequestHeaders.Referrer = uri;
 			return response;
 		}
 
@@ -107,7 +126,7 @@ namespace BerichtManager.IHKClient
 		/// Logs the client in to the IHK website
 		/// </summary>
 		/// <returns><see langword="true"/> if login was successful or client was already logged in and <see langword="false"/> otherwise</returns>
-		internal async Task<bool> DoLogin()
+		private async Task<bool> DoLogin()
 		{
 			if (LoggedIn)
 				return true;
@@ -133,6 +152,221 @@ namespace BerichtManager.IHKClient
 
 			HttpClient.DefaultRequestHeaders.Referrer = FromRelativeUri(uri);
 			return true;
+		}
+
+		/// <summary>
+		/// Creates and saves the report on IHK online campus
+		/// </summary>
+		/// <param name="document"><see cref="Word.Document"/> to upload</param>
+		/// <returns><see langword="true"/> if creation was successful and <see langword="false"/> if not</returns>
+		/// <inheritdoc cref="FillReportContent(Report, HtmlDocument)" path="/exception"/>
+		/// <inheritdoc cref="ReportTransformer.WordToIHK(Word.Document)" path="/exception"/>
+		public async Task<CreateResults> CreateReport(Word.Document document)
+		{
+			if (!LoggedIn)
+				if (!await DoLogin())
+					return CreateResults.Unauthorized;
+
+			//Load list of reports and check session
+			HttpResponseMessage response = await GetAndRefer("tibrosBB/azubiHeft.jsp");
+			if (response.StatusCode == HttpStatusCode.Unauthorized)
+			{
+				LoggedIn = false;
+				//LoggedIn = await DoLogin();
+				return CreateResults.Unauthorized;
+			}
+			//Get new form from IHK
+			response = await PostAndRefer("tibrosBB/azubiHeftEditForm.jsp", new FormUrlEncodedContent(new Dictionary<string, string>() { { "neu", null } }));
+			if (!response.IsSuccessStatusCode)
+				return CreateResults.CreationFailed;
+			//Fill report with contents from new IHK report
+			HtmlDocument doc = GetHtmlDocument(await response.Content.ReadAsStringAsync());
+			Report report = new Report();
+			FillReportContent(report, doc);
+			//Overwrite contents from IHK
+			ReportTransformer.WordToIHK(document, report);
+			MultipartFormDataContent content = GetMultipartFormDataContent(report.ReportContent);
+			//Add necessary save parameter IHK needs to save reports
+			StringContent save = new StringContent("");
+			save.Headers.Remove("Content-Type");
+			content.Add(save, "\"save\"");
+			//Post content to create report
+			response = await PostAndRefer("tibrosBB/azubiHeftAdd.jsp", content);
+			if (response.StatusCode != HttpStatusCode.Found && !response.IsSuccessStatusCode)
+				return CreateResults.UploadFailed;
+			if (response.Headers.Location == null || string.IsNullOrEmpty(response.Headers.Location.ToString()))
+				await GetAndRefer("tibrosBB/azubiHeft.jsp");
+			else
+				await GetAndRefer(response.Headers.Location);
+			return CreateResults.Success;
+		}
+
+		/// <summary>
+		/// Results of the report creation process
+		/// </summary>
+		public enum CreateResults
+		{
+			/// <summary>
+			/// Creation was successful
+			/// </summary>
+			Success,
+			/// <summary>
+			/// Creation failed as session expired
+			/// </summary>
+			Unauthorized,
+			/// <summary>
+			/// Creation failed while fetching creation form from IHK
+			/// </summary>
+			CreationFailed,
+			/// <summary>
+			/// Creation failed while sending form to IHK
+			/// </summary>
+			UploadFailed
+		}
+
+		/// <summary>
+		/// Fills <paramref name="report"/> with form fields from <paramref name="doc"/>
+		/// </summary>
+		/// <param name="report"><see cref="Report"/> which should have its content filled</param>
+		/// <param name="doc"><see cref="HtmlElement"/> of html form</param>
+		/// <exception cref="NoFormFoundException">Thrown if no form element was found in <paramref name="doc"/></exception>
+		/// <exception cref="NoInputsFoundException">Thrown if no input elements were found in <paramref name="doc"/></exception>
+		/// <exception cref="UnknownFieldException">Thrown if a property that is unknown to <see cref="Report"/> is in <paramref name="doc"/></exception>
+		/// <exception cref="InputFieldsMismatchException">Thrown if the length of found inputs does not match the number of properties to fill</exception>
+		private void FillReportContent(Report report, HtmlDocument doc)
+		{
+			if (doc.Forms.Count == 0)
+				throw new NoFormFoundException();
+			if (!FindAllInputInElement(doc.Forms[0], out List<HtmlElement> inputs))
+				throw new NoInputsFoundException();
+			//IHK always sends an extra field which has nothing to do with the report
+			if (inputs.Count - report.ReportContent.GetType().GetProperties().Length > 1)
+			{
+				foreach (HtmlElement htmlElement in inputs)
+				{
+					if (report.ReportContent.GetType().GetProperties().ToList().Find(prop =>
+					{
+						IHKFormDataNameAttribute attr = prop.GetCustomAttributes(typeof(IHKFormDataNameAttribute)).First() as IHKFormDataNameAttribute;
+						if (attr == null)
+							return false;
+						return attr.Name == htmlElement.Name;
+					}) == null)
+						throw new UnknownFieldException(htmlElement.Name);
+				}
+				throw new InputFieldsMismatchException();
+			}
+
+			List<string> filledProps = new List<string>();
+			foreach (HtmlElement input in inputs)
+			{
+				//Find list of properties which have the same IHKForm name as input
+				List<PropertyInfo> matchingProps = report.ReportContent.GetType().GetProperties().ToList().FindAll(prop =>
+				{
+					IHKFormDataNameAttribute attr = prop.GetCustomAttributes(typeof(IHKFormDataNameAttribute)).First() as IHKFormDataNameAttribute;
+					if (attr == null || !attr.IsActuallySent)
+					{
+#if DEBUG
+						Console.WriteLine($"Property {prop.Name} of report has no IHKFormDataNameAttribute");
+#endif
+						return false;
+					}
+
+					return attr.Name == input.Name;
+				});
+				//If no matches found the field is unknown and new to the form
+				if (matchingProps.Count == 0)
+					throw new UnknownFieldException(input.Name);
+				//Fill props with values from input
+				matchingProps.ForEach(prop =>
+				{
+					switch (input.GetAttribute("type").ToLower())
+					{
+						case "file":
+							//Uploading files is not implemented
+							prop.SetValue(report.ReportContent, new byte[0]);
+							break;
+						default:
+							prop.SetValue(report.ReportContent, Convert.ChangeType(input.GetAttribute("value"), prop.PropertyType));
+							break;
+					}
+				});
+			}
+		}
+
+		/// <summary>
+		/// Finds all input and textarea tags in <paramref name="root"/>
+		/// </summary>
+		/// <param name="root"><see cref="HtmlElement"/> root of form or element to check</param>
+		/// <param name="inputs"><see cref="List{T}"/> of <see cref="HtmlElement"/> inputs</param>
+		/// <returns><see langword="true"/> if any inputs were found and <see langword="false"/> otherwise</returns>
+		private bool FindAllInputInElement(HtmlElement root, out List<HtmlElement> inputs)
+		{
+			inputs = new List<HtmlElement>();
+			foreach (HtmlElement element in root.Children)
+			{
+				if (element.TagName.Equals("input", StringComparison.CurrentCultureIgnoreCase) || element.TagName.Equals("textarea", StringComparison.CurrentCultureIgnoreCase))
+					inputs.Add(element);
+				else
+				{
+					if (FindAllInputInElement(element, out List<HtmlElement> childInputs))
+					{
+						inputs.AddRange(childInputs);
+					}
+				}
+			}
+			return inputs.Count > 0;
+		}
+
+		/// <summary>
+		/// Generates a new <see cref="MultipartFormDataContent"/> containing the properties of <paramref name="content"/>
+		/// </summary>
+		/// <param name="content"><see cref="ReportContent"/> content to use</param>
+		/// <returns>New filled <see cref="MultipartFormDataContent"/> object</returns>
+		private MultipartFormDataContent GetMultipartFormDataContent(ReportContent content)
+		{
+			MultipartFormDataContent mPFDC = new MultipartFormDataContent();
+			content.GetType().GetProperties().ToList().ForEach(prop =>
+			{
+				IHKFormDataNameAttribute attr = prop.GetCustomAttributes(typeof(IHKFormDataNameAttribute)).First() as IHKFormDataNameAttribute;
+				if (!attr.IsActuallySent)
+					return;
+				string propName;
+				if (attr != null)
+					propName = attr.Name;
+				else
+					propName = prop.Name;
+
+				if (prop.PropertyType == typeof(byte[]))
+				{
+					ByteArrayContent file = new ByteArrayContent((byte[])prop.GetValue(content) ?? new byte[0]);
+					mPFDC.Add(file, $"\"{propName}\"");
+					file.Headers.Add("Content-Type", "application/octet-stream");
+					file.Headers.ContentDisposition.FileName = "\"\"";
+				}
+				else
+				{
+					StringContent c = new StringContent($"{prop.GetValue(content)}");
+					c.Headers.Remove("Content-Type");
+					mPFDC.Add(c, $"\"{propName}\"");
+				}
+			});
+			return mPFDC;
+		}
+
+		/// <summary>
+		/// Generates an <see cref="HtmlDocument"/> from an HTML string
+		/// </summary>
+		/// <param name="html">HTML string to parse</param>
+		/// <returns>Parsed <see cref="HtmlDocument"/></returns>
+		private HtmlDocument GetHtmlDocument(string html)
+		{
+			WebBrowser browser = new WebBrowser();
+			browser.ScriptErrorsSuppressed = true;
+			browser.DocumentText = html;
+			browser.Document.OpenNew(true);
+			browser.Document.Write(html);
+			browser.Refresh();
+			return browser.Document;
 		}
 	}
 }
