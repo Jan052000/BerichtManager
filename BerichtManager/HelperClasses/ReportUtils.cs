897f04a0a80e16a0b136302d94c36bdc9706c2ad
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-04-23 16:03:38 +0200

    add report utils

---
+++ BerichtManager/HelperClasses/ReportUtils.cs
@@ -0,0 +1,18 @@
+﻿namespace BerichtManager.HelperClasses
+{
+	/// <summary>
+	/// Class holding methods used for interacting with properties of reports
+	/// </summary>
+	internal class ReportUtils
+	{
+		/// <summary>
+		/// Checks wether or not a report is valid based on its full file name and extension
+		/// </summary>
+		/// <param name="name">Full name</param>
+		/// <returns><see langword="true"/> if name is valid and <see langword="false"/> otherwise</returns>
+		public static bool IsNameValid(string name)
+		{
+			return !string.IsNullOrEmpty(name) && !name.StartsWith("~$") && name.EndsWith(".docx");
+		}
+	}
+}
