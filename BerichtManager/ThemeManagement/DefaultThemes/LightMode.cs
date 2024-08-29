b2b668666c4c10a27dc8cbdd42bebba43865a276
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-08-29 14:07:54 +0200

    improve light theme colors

--- BerichtManager/ThemeManagement/DefaultThemes/LightMode.cs
+++ BerichtManager/ThemeManagement/DefaultThemes/LightMode.cs
@@ -14,12 +14,12 @@ namespace BerichtManager.ThemeManagement.DefaultThemes
 		public Color ColoredComboBoxTextColor => Color.Black;
 		public Color ColoredComboBoxDisabledColor => Color.FromArgb(109, 109, 109);
 		public Color ColoredComboBoxDisabledTextColor => Color.FromArgb(109, 109, 109);
-		public Color ColoredComboBoxHighlightColor => Color.FromArgb(0, 120, 215);
-		public Color MenuStripBackColor => Color.FromArgb(255, 255, 255);
+		public Color ColoredComboBoxHighlightColor => Color.FromArgb(100, 150, 240);
+		public Color MenuStripBackColor => Color.FromArgb(240, 240, 240);
 		public Color MenuStripDropdownBackColor => Color.FromArgb(240, 240, 240);
-		public Color MenuStripSelectedDropDownBackColor => Color.FromArgb(240, 240, 240);
-		public Color ForeColor => Color.FromArgb(0, 0, 0);
-		public Color BackColor => Color.FromArgb(240, 240, 240);
+		public Color MenuStripSelectedDropDownBackColor => Color.FromArgb(100, 150, 240);
+		public Color ForeColor => Color.Black;
+		public Color BackColor => Color.White;
 		public Color ButtonColor => Color.LightGray;
 		public Color ButtonDisabledColor => Color.FromArgb(235, 235, 228);
 		public Color ButtonDisabledTextColor => Color.Gray;
@@ -28,7 +28,7 @@ namespace BerichtManager.ThemeManagement.DefaultThemes
 		public Color ButtonHoverColor => Color.FromArgb(229, 241, 251);
 		public Color SplitterColor => Color.FromArgb(150, 150, 150);
 		public Color TreeViewDottedLineColor => Color.Gray;
-		public Color TreeViewHighlightedNodeColor => Color.FromArgb(0, 120, 215);
+		public Color TreeViewHighlightedNodeColor => Color.FromArgb(100, 150, 240);
 		public Color TreeViewReportOpenedHighlightColor => Color.FromArgb(0, 120, 255);
 		public Color ReportUploadedColor => Color.Blue;
 		public Color ReportHandedInColor => Color.FromArgb(242, 222, 0);
