0a42b72f814d70ff411000dc406cd00d7413a93d
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2024-10-10 15:05:20 +0200

    v1.19

--- BerichtManager/MainForm.cs
+++ BerichtManager/MainForm.cs
@@ -68,7 +68,7 @@ namespace BerichtManager
 		/// Version number
 		/// Major.Minor.Build.Revision
 		/// </summary>
-		public const string VersionNumber = "1.18.4";
+		public const string VersionNumber = "1.19";
 
 		/// <summary>
 		/// String to be printed
