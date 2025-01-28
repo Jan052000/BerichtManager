ef30dc1064839bc1c62966150d56f57ea941e58a
Jan-Lukas Jöhren <jan-lukas.joehren@mum.de>
2025-01-28 08:35:11 +0100

    v1.21

--- BerichtManager/MainForm.cs
+++ BerichtManager/MainForm.cs
@@ -70,7 +70,7 @@ namespace BerichtManager
 		/// Version number
 		/// Major.Minor.Build.Revision
 		/// </summary>
-		public const string VersionNumber = "1.20.1";
+		public const string VersionNumber = "1.21";
 
 		/// <summary>
 		/// String to be printed
